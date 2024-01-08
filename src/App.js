97d111751a465329921b6f82f3f3d39de9ac5564
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-01-08 23:25:48 +0300

    useState and useEffect used for char and search values

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [char, setChar] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const changeHandler = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setChar(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [search]);
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <Header></Header>
     </div>
   );
-}
+};
 
 export default App;

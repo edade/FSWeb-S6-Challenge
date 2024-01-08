e066e0a69801084d080b672e451d23b5cb656e96
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-01-09 00:00:46 +0300

    charakter infos displayed in the page

--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,25 @@
-// Karakter bileşeniniz buraya gelecek
+import React from "react";
+import { AccordionBody, AccordionHeader, AccordionItem } from "reactstrap";
+import styled from "styled-components";
+import Filmler from "./Filmler";
+
+export default function Karakter(props) {
+  const { id, character } = props;
+  const styDiv = styled.div`
+    font-size: 2rem;
+  `;
+
+  return (
+    <div>
+      <AccordionItem>
+        <AccordionHeader targetId={id}>{character.name}</AccordionHeader>
+        <AccordionBody accordionId={id}>
+          <styDiv>Gender: {character.gender} </styDiv>
+          <styDiv>Height: {character.height}</styDiv>
+          <styDiv>Mass: {character.mass}</styDiv>
+          <Filmler />
+        </AccordionBody>
+      </AccordionItem>
+    </div>
+  );
+}
